91b0aadd6cf7c9e68501019cf4609fb9c274adda
Dolev-Tz <dolevtzviel123@gmail.com>
2024-11-01 22:53:37 +0200

    Update python.groovy

--- python.groovy
+++ python.groovy
@@ -1,6 +1,6 @@
 job('HelloWorldJob') {
     description('A simple job that echoes Hello World')
     steps {
-        shell('echo "Hello World"')     
+        batchfile('echo "Hello World"')     
     }
 }
